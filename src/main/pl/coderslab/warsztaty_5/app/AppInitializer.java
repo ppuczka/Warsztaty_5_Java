a7f9d5218a6a659b009e8b1b89f0174dc36c8f7d
p.puczka <p.puczka@gmail.com>
2018-10-06 14:40:59 +0200

    adding model

---
+++ src/main/pl/coderslab/warsztaty_5/app/AppInitializer.java
@@ -0,0 +1,36 @@
+package pl.coderslab.warsztaty_5.app;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() { return null; }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};  }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};   }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter =
+                new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[] { characterEncodingFilter };
+    }
+}
+
+
---
+++ src/main/pl/coderslab/warsztaty_5/controller/BookController.java
@@ -0,0 +1,4 @@
+package pl.coderslab.warsztaty_5.controller;
+
+public class BookController {
+}
